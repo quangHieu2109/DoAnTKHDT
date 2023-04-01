61bb7d9db38ac3ecad9cf5d081aaefa5ca76900a
hai000 <128612861+hai000@users.noreply.github.com>
2023-04-01 22:01:04 +0700

    Update Player.java

--- src/Models/Player.java
+++ src/Models/Player.java
@@ -1,5 +1,46 @@
 package models;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+	String name;
+	int money;
+	List<Bai> baiTrenTay;
+
+	public Player(String name, int money) {
+		this.name = name;
+		this.money = money;
+		baiTrenTay = new ArrayList<>();
+	}
+
+	public void themBai(Bai bai) {
+		baiTrenTay.add(bai);
+	}
+
+	public int getDiemBaiTrenTay() {
+		int value = 0;
+		for (Bai bai : baiTrenTay) {
+			if (bai.getSo() > 10) {
+				value += 10;
+			} else {
+				value += bai.getSo();
+			}
+		}
+		return value;
+	}
+
+	public List<Bai> getBaiTrenTay() {
+		return baiTrenTay;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMoney() {
+		return money;
+	}
 }
+
